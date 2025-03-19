343d6ec34982b372d6e53a570e9653b95a8dd484
michel-lewis <kouamkouammichellewis@gmail.com>
2025-03-19 07:30:14 +0100

    correction traduction

--- gofr-gui/src/components/side-menu.vue
+++ gofr-gui/src/components/side-menu.vue
@@ -52,7 +52,7 @@
           >
             <template v-slot:activator>
               <v-list-item-title class="subtitle-1 font-weight-bold text-uppercase">
-                {{ item.text === "App.menu.account.msg" ? "Compte" : $t(`App.menu.${item.text}`) }}
+                {{  $t(`App.menu.${item.text}`) === "App.menu.App.menu.account.msg" ? $t('App.menu.Account') : $t(`App.menu.${item.text}`) }}
               </v-list-item-title>
             </template>
             <template v-for="sub in item.menu">

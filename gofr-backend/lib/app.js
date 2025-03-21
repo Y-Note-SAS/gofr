cdcf9e3f284a98c9c0a35e800a9ed1b7ff593669
michel-lewis <kouamkouammichellewis@gmail.com>
2025-03-21 12:30:48 +0100

    console.log values

--- gofr-backend/lib/app.js
+++ gofr-backend/lib/app.js
@@ -86,7 +86,9 @@ async function startUp() {
     if (config.get('app:idp') === 'keycloak') {
       if (req.cookies && req.cookies.userObj) {
         req.user = user.restoreUser(JSON.parse(req.cookies.userObj));
-        console.log("req.user 1", req.user.permissions);
+        console.log("req.user 1", req.user.permissions.partitions);
+        console.log("req.user 3", req.user.permissions);
+
         return keycloak.protect()(req, res, next);
       }
       // for backend processes making API Calls

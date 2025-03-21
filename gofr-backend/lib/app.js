6c9581af92f09419f618b5da95d3183192143657
michel-lewis <kouamkouammichellewis@gmail.com>
2025-03-21 17:44:11 +0100

    log ressource

--- gofr-backend/lib/app.js
+++ gofr-backend/lib/app.js
@@ -89,6 +89,7 @@ async function startUp() {
     if (config.get('app:idp') === 'keycloak') {
       if (req.cookies && req.cookies.userObj) {
         console.log("req.cookies.userObj", req.cookies.userObj);
+        console.log("resource dhir roles ",req.user.updatePermissions());
         req.user = user.restoreUser(JSON.parse(req.cookies.userObj));
         console.log("req.user 1", req.user.permissions.partitions);
         console.log("req.user 3", req.user.permissions);
